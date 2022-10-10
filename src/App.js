c344af96377a4d5ca1da66262167c56d81272bcc
John Doe <johndoe@example.com>
2022-10-10 13:35:32 +0700

    eiei67

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React EIEI6
+          Learn React EIEI67
         </a>
       </header>
     </div>
